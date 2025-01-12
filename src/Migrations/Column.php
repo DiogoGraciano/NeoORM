c6e04355e55dfbfb94330e226abc7068b58c870a
Diogo Graciano <138625607+DiogoGraciano@users.noreply.github.com>
2025-01-12 17:38:36 -0300

    Ajuste nome arquivos

---
+++ src/Migrations/Column.php
@@ -0,0 +1,60 @@
+<?php
+namespace Diogodg\Neoorm\Migrations;
+
+use Diogodg\Neoorm\Migrations\Driver\ColumnMysql;
+use Diogodg\Neoorm\Migrations\Driver\ColumnPgsql;
+use Diogodg\Neoorm\Migrations\Interface\Column as ColumnInterface;
+
+/**
+ * Classe base para criação do banco de dados.
+ */
+class Column implements ColumnInterface
+{
+    private object $column;
+
+    public function __construct(string $name,string $type,string|int|null $size = null)
+    {
+        $env = parse_ini_file('.env');
+
+        if($env["DRIVER"] == "mysql"){
+            $this->column = new columnMysql($name,$type,$size);
+        }
+        elseif($env["DRIVER"] == "pgsql"){
+            $this->column = new columnPgsql($name,$type,$size);
+        }
+    }
+
+    public function isNotNull(){
+        $this->column->isNotNull();
+        return $this;
+    }
+
+    public function isPrimary(){
+        $this->column->isPrimary();
+        return $this;
+    }
+
+    public function isUnique(){
+        $this->column->isUnique();
+        return $this;
+    }
+
+    public function isForeingKey(table $foreingTable,string $foreingColumn = "id"){
+        $this->column->isForeingKey($foreingTable,$foreingColumn);
+        return $this;
+    }
+
+    public function setDefaut(string|int|float|null $value = null,bool $is_constant = false){
+        $this->column->setDefaut($value,$is_constant);
+        return $this;
+    }
+
+    public function getColumn(){
+        return $this->column->getColumn();
+    }
+
+    public function setComment($comment){
+        $this->column->setComment($comment);
+        return $this;
+    }
+}
---
+++ src/Migrations/Interface/Table.php
@@ -0,0 +1,31 @@
+<?php
+namespace Diogodg\Neoorm\Migrations\Interface;
+
+/**
+ * Classe base para criação do banco de dados.
+ */
+interface Table
+{
+   
+    function __construct(string $table,string $engine="InnoDB",string $collate="utf8mb4_general_ci",string $comment = "");
+
+    public function isAutoIncrement():self;
+
+    public function getAutoIncrement():bool;
+
+    public function addIndex(string $name,array $columns):self;
+
+    public function create();
+
+    public function execute($recreate = false);
+
+    public function hasForeignKey():bool;
+
+    public function getForeignKeyTablesClasses():array;
+
+    public function getTable():string;
+
+    public function getColumnsName():array;
+    
+    public function exists():bool;
+}
